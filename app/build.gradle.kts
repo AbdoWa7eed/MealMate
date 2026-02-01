b3473d55c4615b6cee8470195ea212bf0c5123c7
Wa7eed <abdo20132233@gmail.com>
2026-02-01 14:06:37 +0200

    Integrate Firebase Authentication and configure Google Services
    - Add Firebase BOM and Firebase Auth dependencies to `libs.versions.toml`.
    - Configure the Google Services plugin in both the top-level and app-level `build.gradle.kts` files.
    - Implement Firebase Auth and BOM dependencies in the app-level `build.gradle.kts`.
    - Update `.gitignore` to exclude `google-services.json`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,4 +51,8 @@ dependencies {
     runtimeOnly(libs.core.splashscreen)
     implementation(libs.navigation.fragment)
     implementation(libs.dotsindicator)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+
+
 }
